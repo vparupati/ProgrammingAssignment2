701093b24abf6decd6c7d27cfacba870a669d3ed
vparupati <sagarreddy@hotmail.co.uk>
2015-06-21 23:42:41 +0100

    dding inverted cahce matrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Functions for creating and using cacheble inverted matrices 
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  # validate matrix
+  if (!is.matrix(x)) {
+    stop("Agument is not a matrix")
+  }
+  
+  invertedMatrix <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    invertedMatrix <<- NULL
+  }
+  
+  # Functions for getting and setting cached inv. matrix value
+  get <- function() x
+  # Inversing the matrix using build in solve() function in R
+  set.inverse <- function(solve) invertedMatrix <<- solve
+  get.inverse <- function() invertedMatrix
+  
+  list(
+    set = set, 
+    get = get,
+    set.inverse = set.inverse,
+    get.inverse = get.inverse)
 
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+  
+  invertedMatrix <- cacheableMatrix$get.inverse()
+  # getting cached inverted matrix
+  if(!is.null(invertedMatrix)) {
+    return(invertedMatrix)
+  }
+  #create inverted matrix and cache it
+  matrixToInverse <- cacheableMatrix$get()
+  invertedMatrix <- solve(matrixToInverse)
+  cacheableMatrix$set.inverse(invertedMatrix)
+  invertedMatrix
+  
 }
+
